b6956aa8d361263614b68075a1abf2195e09b8f9
Neonsy <neonsy@users.noreply.github.com>
2024-09-18 14:25:15 +0200

    Added Inter as the Font

--- Project-01/app/src/App.tsx
+++ Project-01/app/src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-    return <></>;
+    return <><h1>WhatFont Am I?</h1></>;
 }
 
 export default App;
--- Project-01/app/src/main.tsx
+++ Project-01/app/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './main.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App.tsx';
+
+import './main.css';
+import './assets/fonts/inter/inter.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
