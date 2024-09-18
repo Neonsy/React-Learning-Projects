6fd20e4dcca74ac37949e18f26a71a50ddb99dac
Neonsy <neonsy@users.noreply.github.com>
2024-09-18 14:36:08 +0200

    Added FontAwesome

--- Project-01/app/src/App.tsx
+++ Project-01/app/src/App.tsx
@@ -1,5 +1,16 @@
+import { faStarHalfStroke } from '@fortawesome/free-regular-svg-icons/faStarHalfStroke';
+import { faLocationPin, faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 function App() {
-    return <><h1>WhatFont Am I?</h1></>;
+    return (
+        <>
+            <h1>WhatFont Am I?</h1>
+            <FontAwesomeIcon icon={faLocationPin} />
+            <FontAwesomeIcon icon={faStar} color='#1c1cfe' />
+            <FontAwesomeIcon icon={faStarHalfStroke} color='orange' />
+        </>
+    );
 }
 
 export default App;
