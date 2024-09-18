cac66b77e416565bd02323eea31750ab13e65885
Neonsy <neonsy@users.noreply.github.com>
2024-09-18 15:35:20 +0200

    Basic Router-Setup complete

--- Project-01/app/src/components/header/nav/index.tsx
+++ Project-01/app/src/components/header/nav/index.tsx
@@ -1,7 +1,21 @@
+import { Link } from '@tanstack/react-router';
+
 export default function Nav() {
     return (
-        <nav>
+        <nav className='w-full flex justify-around'>
             <h1>Pretending to be a nav... for now</h1>
+            <ul className='flex gap-x-5'>
+                <li>
+                    <Link className='hover:underline [&.active]:font-bold' to='/'>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link className='hover:underline [&.active]:font-bold' to='/about'>
+                        About
+                    </Link>
+                </li>
+            </ul>
         </nav>
     );
 }
