3a42a35bdf14a50a5430fd116607200e39246875
Neonsy <neonsy@users.noreply.github.com>
2024-10-15 10:06:46 +0200

    Working on App Page

--- Task Snap Space/src/pages/app/index.tsx
+++ Task Snap Space/src/pages/app/index.tsx
@@ -1,7 +1,20 @@
 export default function App() {
     return (
-        <main>
-            <h1>App Page</h1>
+        <main className='flex flex-col gap-y-5 mt-5'>
+            <section className='bg-white/90 px-5 py-3'>
+                <h2 className='font-semibold'>Add New Task</h2>
+                <form className='flex gap-x-3'>
+                    <input className='flex-grow' placeholder='Enter your Task here' />
+                    <select>
+                        <option>To Do</option>
+                        <option>In Progress</option>
+                    </select>
+                    <button type='submit'>Add</button>
+                </form>
+            </section>
+            <section className='bg-white/90 px-5 py-3'>
+                <h2 className='font-bold'>Your Task Dashboard</h2>
+            </section>
         </main>
     );
 }
